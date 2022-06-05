3c12f804c754ab7539eddf17805362596c18af36
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-05 10:49:35 -0500

    Init commit

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<main>
+  <h1>Hello world!</h1>
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 480px) {
+  }
+  @media (min-width: 768px) {
+  }
+  @media (min-width: 1024px) {
+  }
+  @media (min-width: 1200px) {
+  }
+</style>
