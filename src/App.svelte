7990ef07f1fa4116ec45bc5cf3604f2f3bb4d7fd
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-07 05:53:47 -0400

    add to see

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,22 @@
 <script>
   import "chota";
+  let hidden = false;
   import { Confetti } from "svelte-confetti";
   import { darkmode } from "./lib/store.js";
   import Search from "./lib/Search.svelte";
-  export let menu = 1;
+  import Card from "./lib/Card.svelte";
   let nonsense = false;
   let courses = [
-    { id: 1, name: "Pebble Beach", par: 71 },
-    { id: 2, name: "Sawgrass", par: 71 },
-    { id: 3, name: "portrush", par: 71 },
-    { id: 4, name: "pinehurst", par: 71 },
-    { id: 5, name: "mangrove bay", par: 71 },
-    { id: 6, name: "doral", par: 71 },
-    { id: 7, name: "jupiter beach", par: 71 },
-    { id: 8, name: "Sawgrass", par: 71 },
-    { id: 9, name: "Sawgrass", par: 71 },
-    { id: 10, name: "Sawgrass", par: 71 }
+    { id: 1, name: "Pebble Beach", par: 71, drivingRange: false },
+    { id: 2, name: "Sawgrass", par: 71, drivingRange: false },
+    { id: 3, name: "portrush", par: 71, drivingRange: false },
+    { id: 4, name: "pinehurst", par: 71, drivingRange: false },
+    { id: 5, name: "mangrove bay", par: 71, drivingRange: false },
+    { id: 6, name: "doral", par: 71, drivingRange: false },
+    { id: 7, name: "jupiter beach", par: 71, drivingRange: false },
+    { id: 8, name: "Sawgrass", par: 71, drivingRange: false },
+    { id: 9, name: "Sawgrass", par: 71, drivingRange: false },
+    { id: 10, name: "Sawgrass", par: 71, drivingRange: false }
   ];
 </script>
 
@@ -31,41 +32,27 @@
   </div>
 </nav>
 
-{#if menu === 1}
-  <main class="">
-    <div class="test">
-      <Search />
-      <span class="hide-xs hide-sm">
-        <input
-          type="checkbox"
-          name="nonsenseToggle"
-          id=""
-          bind:checked={nonsense}
-        />
-        <label for="nonsenseToggle">Nonsense?</label>
-      </span>
-    </div>
-    <!-- list of course sections -->
-    {#each courses as course, index}
-      <div class="card">
-        <header>
-          <h4>{course.name}</h4>
-        </header>
-        <p>Par {course.par}</p>
-        <footer class="is-right">
-          <a class="button">Cancel</a>
-          <a class="button primary">Start Round</a>
-        </footer>
-        {course.name}
-      </div>
-    {:else}
-      <span>We aren't able to see any courses nearby.</span>
-    {/each}
-  </main>
-{:else if menu === 2}
-  See yo stats!
-{:else}
-  signout modal{/if}
+<main class="">
+  <div class="test">
+    <Search />
+    <span class="hide-xs hide-sm">
+      <input
+        type="checkbox"
+        name="nonsenseToggle"
+        id=""
+        bind:checked={nonsense}
+      />
+      <label for="nonsenseToggle">Nonsense?</label>
+    </span>
+  </div>
+
+  <!-- list of course sections -->
+  {#each courses as course (course.id)}
+    <Card {course} />
+  {:else}
+    <span>We aren't able to see any courses nearby.</span>
+  {/each}
+</main>
 
 <style>
   :root {
@@ -82,10 +69,6 @@
     justify-content: space-between;
   }
 
-  #menu li {
-    display: inline;
-  }
-
   /* 
   @media (min-width: 1200px) {
   }
@@ -93,9 +76,57 @@
   }
   @media (min-width: 768px) {
   }*/
-  @media (min-width: 480px) {
+  /* @media screen and (min-width: 375px) {
     main {
-      background-color: red;
+      background-color: darkblue;
+    }
+  } */
+
+  @media (prefers-color-scheme: dark) {
+    .dark-scheme {
+      background: #333;
+      color: white;
+    }
+    .dark-scheme {
+      background: black;
+      color: #ddd;
     }
   }
+
+  @media (prefers-color-scheme: light) {
+    .light-scheme {
+      background: white;
+      color: #555;
+    }
+    .light-scheme {
+      background: #eee;
+      color: black;
+    }
+  }
+
+  .day,
+  .night {
+    display: inline-block;
+    padding: 1em;
+    width: 7em;
+    height: 2em;
+    vertical-align: middle;
+  }
+  /* .dark {
+    --bg-color: #000;
+    --bg-secondary-color: #131316;
+    --font-color: #f5f5f5;
+    --color-grey: #ccc;
+    --color-darkGrey: #777;
+  } */
+  /* 
+  .glowing {
+    color: white;
+    text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;
+  }
+
+  .bordered_text {
+    color: white;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  } */
 </style>
