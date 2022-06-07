a4b210fd23450c293e0f7af910e681e0b7d548c7
Matt C <42080317+leafsicle@users.noreply.github.com>
2022-06-07 04:19:26 -0400

    added store writable and cleared readme

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,5 @@
-import { writable } from 'svelte/store';
+import { writable } from "svelte/store";
+export default writable(0);
 
 export const darkmode = writable(true);
 export const userLoggedIn = writable(true);
